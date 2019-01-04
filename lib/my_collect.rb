16c26683c3d43fd805840207ab4cc4615e68c63e
Joe Mullen <mullenjoe13@gmail.com>
2019-01-04 22:02:11 +0000

    Done.

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,2 +1,17 @@
+def my_collect(arr)
+  i=0
+  new_arr = []
+  authenticate_or_request_with_http_digestarr=[]
+    while i<arr.length
+    new_arr << (yield arr[i])
+    i+=1
+  end
+  new_arr
+end
 
 
+
+#array = ["Tim Jones", "Tom Smith", "Jim Campagno"]
+#my_collect(array) do |name|
+ # name.split(" ").first
+#end
